14c2ce7fad33f2a0df89ae6481ea2ad69f1abcaf
Chris <christoph.hoeppke@gmail.com>
2018-08-13 16:26:35 +0100

    updates
    Signed-off-by: Chris <christoph.hoeppke@gmail.com>

--- R/maxnodf.R
+++ R/maxnodf.R
@@ -1,18 +1,33 @@
 #' Compute NODF-maximising graph
 #'
-#' @param NodesA    The number of rows in the desired network.
-#' @param NodesB    The number of columns in the desired network.
-#' @param Edges     The number of Edges in the desired network.
 #' @param quality   An optional quality parameter to control the
 #'                  tradeoff between computation time and result quality.
 #' @return Approximation of the NODF maximising graph.
 #' @examples
 #' maxnodf(14, 13, 52)
 #' maxnodf(14, 13, 52, 2)
-maxnodf <- function(NodesA, NodesB, Edges, quality = 0){
+maxnodf <- function(web, quality = 0){
     #' @useDynLib maxnodf
     #' @import Rcpp
     #' @export
+    web[web>0] <- 1
+    if(any(web < 0)){
+        stop("Invalid network. Ensure all elements of web >= 0.")
+    }
+    if( !quality %in% 0:2){
+        stop("Please chose a valid quality parameter. Options: \n\tquality = 0 -> Use a very fast greedy algorithm.\n\tquality = 1 -> Improved result using hillclimbing in combination with greedy.\n\tquality = 2 -> Use a simulated annealing algorith. Best results but requires the most computation time")
+    }
+    mt_0  <- computeMT0(web)
+    mt_t  <- computeMT0(web)
+    if(any(mt_0) < 1){
+        stop("Invalid network. Ensure all marginal totals are >= 1.")
+    }
+    if(any(mt_t) < 1){
+        stop("Invalid network. Ensure all marginal totals are >= 1.")
+    }
+    NodesA <- nrow(web)
+    NodesB <- ncol(web)
+    Edges <- sum(web)
     if(quality == 0){
         # Use a basic greedy algorithm to find the optimum
         mtx <- greedy_solve2(NodesA, NodesB, Edges)
@@ -26,12 +41,7 @@ maxnodf <- function(NodesA, NodesB, Edges, quality = 0){
         # improved solution.
         mtx <- greedy_solve2(NodesA, NodesB, Edges)
         mtx <- sim_anneal_opt_cpp(mtx)
-    }else{
-        print("Please chose a valid quality parameter. Options: ")
-        print("quality = 0 -> Use a very fast greedy algorithm.")
-        print("quality = 1 -> Improved result using hillclimbing in combination with greedy.")
-        print("quality = 2 -> Use a simulated annealing algorith. Best results but requires the most computation time")
     }
-    return(mtx)
+    return(list(nodf_cpp(mtx), mtx))
 }
 
